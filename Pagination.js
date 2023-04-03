e354e41ec3c54dc0c99086efa47642f132f070c3
TaronKarapetyan <taronvkarapetyan@gmail.com>
2023-04-03 13:44:56 +0500

    fix(it needs to be 'return Pagination) so, where is hidden the dog's head

--- Pagination.js
+++ Pagination.js
@@ -2,54 +2,68 @@ const alphabetArray = "abcdefghijklmnopqrstuvwxyz".split("")
 let currentPage = 0
 const Pagination = {
     init(array, pageSize) {
-        const arr =[]
-        for(let i = 0; i < array.length; i += pageSize) {
-            arrLast = []            
-            for(let j = 0; j < pageSize; j++){
-                if(typeof array[j+i] !== 'undefined') {
-                    arrLast.push(array[j+i])
-                }                
+        let arr =[]
+        let subArr = []
+        for(let i = 0; i < array.length; i++) {
+            subArr.push(array[i])
+            if(subArr.length === pageSize || i === array.length - 1) {
+                arr.push(subArr)
+                subArr = []
             }
-            arr.push(arrLast)            
+            // for(let j = 0; j < pageSize; j++){
+            //     if(typeof array[j+i] !== 'undefined') {
+            //         arrLast.push(array[j+i])
+            //     }                
+            // }           
     }
         return arr  // our book is ready
     },
     prevPage() {
         if(currentPage) {
-            currentPage -=1
+            currentPage -= 1
         }
+        return Pagination
     },
     nextPage() {
-        if(currentPage < book.length) {
-        return currentPage +=1
+        if(currentPage < book.length - 1) {
+        currentPage += 1
         }
+        return Pagination
     },
     firstPage() {
-        return currentPage = 0
+        currentPage = 0
+        return Pagination
     },
     lastPage() {
-        return currentPage = book.length - 1
+        currentPage = book.length - 1
+        return Pagination
     },
     goToPage(numOfPage) { 
         if(numOfPage < book.length) {
-            return currentPage = numOfPage
+            currentPage = numOfPage
         }
+        return Pagination
     },
     getPageItem(){
         return book[currentPage]
     },
 
 }
-const book = Pagination.init(alphabetArray, 5)
-console.log(book)
-console.log(Pagination.getPageItem())
-Pagination.nextPage()
-console.log(Pagination.getPageItem())
-Pagination.lastPage()
-console.log(Pagination.getPageItem())
-Pagination.goToPage(3)
-console.log(Pagination.getPageItem())
-Pagination.firstPage()
-console.log(Pagination.getPageItem())
-Pagination.prevPage()
-console.log(Pagination.getPageItem())
+const book = Pagination.init(alphabetArray, 4)
+ console.log(book)
+ console.log(Pagination.getPageItem())
+ Pagination.nextPage()
+ console.log(Pagination.getPageItem())
+ Pagination.lastPage()
+ console.log(Pagination.getPageItem())
+ Pagination.goToPage(3)
+ console.log(Pagination.getPageItem())
+ Pagination.firstPage()
+ console.log(Pagination.getPageItem())
+ Pagination.prevPage()
+ console.log(Pagination.getPageItem())
+ Pagination.nextPage().nextPage()
+ console.log(Pagination.getPageItem())
+ Pagination.nextPage().nextPage().nextPage().nextPage().nextPage().nextPage()
+ console.log(Pagination.getPageItem())
+
